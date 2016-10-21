c25c9db2ae7620e84d328aefb12229a8a0d350ec
Davi <xdavipereira@gmail.com>
2016-10-21 01:08:22 -0300

    Add redcarpet gem and pigments.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	def markdown(content) 
+		renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+		options = {
+			autolink: true,
+			no_intra_emphasis: true,
+			disable_idented_code_blocks: true,
+			fenced_code_blocks: true,
+			lax_html_blocks: true,
+			superscript: true
+		}
+
+		Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+	end
 end
